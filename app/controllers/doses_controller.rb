d8eceb7ae0824d2cb98df3a1bced69f385f77bea
Berkay Kabay <mail@berkaykabay.de>
2019-08-16 12:41:53 +0200

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.ingredient = @ingredient
+    @dose.save
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      raise
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktail_ingredients = open(url).read
+ingredients = JSON.parse(cocktail_ingredients)
+ingredients['drinks'].each do |ing|
+  Ingredient.create(name: ing["strIngredient1"])
+end
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Gin Tonic")
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Whiskey Sour")
